5f7b963589cbb7be3a0e6469c813e04858049ecb
walter <walteryu@126.com>
2016-05-19 23:42:50 +0800

    增加日志及相关配置，增加index启动文件，增加用户逻辑层

---
+++ src/index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by way on 3/7/16.
+ */
+
+const debug = require('debug')('koastart');
+import path from 'path';
+import cfg from '../config/app';
+import fs from 'fs';
+import log4js from 'log4js';
+import StatusError from './utils/StatusError';
+import app from './app';
+
+
+/*
+ * 创建日志目录
+ */
+try {
+  if (!fs.existsSync('./log'))
+    fs.mkdirSync('./log');
+
+  if (!fs.existsSync('./log/err'))
+    fs.mkdirSync('./log/err');
+
+  if (!fs.existsSync('./log/hour'))
+    fs.mkdirSync('./log/hour');
+
+  if (!fs.existsSync('./log/http'))
+    fs.mkdirSync('./log/http');
+} catch (e) {
+  console.error('Create log directory exp: %s', e.message);
+}
+
+const log = log4js.getLogger('index');
+// 日志配置加载，每隔 300秒检查设置变更！
+log4js.configure('./config/log4js.json', { reloadSecs: 300 });
+
+async function main() {
+  const port = process.env.PORT || cfg.app.port;
+  const host = process.env.HOST || '';
+
+  await new Promise((res, rej) => {
+    const server = app.listen(port, host, res);
+    server.on('error', rej);
+  });
+
+  log.info('koa start on port: %s', port);
+}
+
+// 启动服务!
+main().catch(err => setImmediate(() => { throw err; }));
+
---
+++ src/utils/StatusError.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Yun on 2015-12-19.
+ */
+
+const MESSAGES = {
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not found',
+  409: 'Conflict',
+  500: 'Server Internal'
+};
+
+export default class StatusError extends Error {
+  constructor(code, message) {
+    super(message || MESSAGES[code]);
+    this.code = code;
+  }
+}
